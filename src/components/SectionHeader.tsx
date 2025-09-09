fabf8b7958383db1076c1d9882a2efece7366b34
nhl1991 <laslark1991@gmail.com>
2025-09-09 23:54:09 +0900

    Fix Link in AnimationNode

--- src/components/SectionHeader.tsx
+++ src/components/SectionHeader.tsx
@@ -2,7 +2,7 @@ export default function SectionHeader({ children }: { children: string }) {
 
     return (
         <>
-            <h1 className="text-[clamp(2rem,2rem+1vw,3rem)] fill-transparent bg-clip-text text-transparent bg-gradient-to-r from-purple-400 via-pink-500 to-sky-500">{children}</h1>
+            <h1 className="text-[clamp(2rem,2rem+1vw,3rem)] fill-transparent bg-clip-text text-transparent bg-gradient-to-t from-white via-sky-500 to-indigo-500">{children}</h1>
         </>
     )
 }
