5f0bc32a03bbe3609c6cf666fa23756c37556d47
Laslark <laslark1991@gmail.com>
2025-12-16 16:35:49 +0900

    Fix build error

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,16 +1,13 @@
 "use client";
 import HomeNavigation from "./_components/HomeNavigation";
 import HomeHero from "./_components/HomeHero";
-import { useState } from "react";
 
 export default function Home() {
-  const [ isComplete, setIsComplete ] = useState<boolean>(false);
-  const onLoadComplete = () => setIsComplete(true);
 
   return (
     <section className="min-h-screen w-full md:max-w-3xl flex items-center justify-center relative">
       <div className="w-full h-[30rem] flex flex-col gap-y-4 items-center justify-center relative">
-        <HomeHero onComplete={onLoadComplete} />
+        <HomeHero />
         <HomeNavigation />
       </div>
     </section>
