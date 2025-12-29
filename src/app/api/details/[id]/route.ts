5bc207b18d53b4d46d8fb1c880e6b57b8816dedb
Laslark <laslark1991@gmail.com>
2025-12-29 16:32:34 +0900

    chore: normalize cache tag values

--- src/app/api/details/[id]/route.ts
+++ src/app/api/details/[id]/route.ts
@@ -5,7 +5,8 @@ export async function GET(req: NextRequest) {
     const {pathname } = req.nextUrl;
     const id = pathname.split('/')[3];
     
-    const result = await getAnimations(`anime/${id}?fields=id,title,main_picture,alternative_titles,start_date,end_date,synopsis,mean,rank,popularity,num_list_users,num_scoring_users,nsfw,created_at,updated_at,media_type,status,genres,my_list_status,num_episodes,start_season,broadcast,source,average_episode_duration,rating,pictures,background,related_anime,related_manga,recommendations,studios,statistics`, 'details')
+    const result = await getAnimations(`anime/${id}?fields=id,title,main_picture,alternative_titles,start_date,end_date,synopsis,mean,rank,popularity,num_list_users,num_scoring_users,nsfw,created_at,updated_at,media_type,status,genres,my_list_status,num_episodes,start_season,broadcast,source,average_episode_duration,rating,pictures,background,related_anime,related_manga,recommendations,studios,statistics`, 
+      `details/${id}`)
      if (result.ok) {
         const data = await result.json();
         return NextResponse.json(data, { status: result.status });
