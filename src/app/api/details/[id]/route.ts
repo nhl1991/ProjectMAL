9ff0e00dece1181cc97f92ed81f6d3d52ea9f62a
Laslark <laslark1991@gmail.com>
2025-12-29 15:58:49 +0900

    chore : Add user rating at '/details'

--- src/app/api/details/[id]/route.ts
+++ src/app/api/details/[id]/route.ts
@@ -2,8 +2,8 @@ import { getAnimations } from "@/lib/fetchAnimation";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(req: NextRequest) {
-    const {pathname } =req.nextUrl
-    const id = pathname.split('/')[3]
+    const {pathname } = req.nextUrl;
+    const id = pathname.split('/')[3];
     
     const result = await getAnimations(`anime/${id}?fields=id,title,main_picture,alternative_titles,start_date,end_date,synopsis,mean,rank,popularity,num_list_users,num_scoring_users,nsfw,created_at,updated_at,media_type,status,genres,my_list_status,num_episodes,start_season,broadcast,source,average_episode_duration,rating,pictures,background,related_anime,related_manga,recommendations,studios,statistics`, 'details')
      if (result.ok) {
