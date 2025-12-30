aa6124aa708bd82081ea73c5ec333ff369e49307
nhl1991 <laslark1991@gmail.com>
2025-12-30 17:56:54 +0900

    chore: lp image optimization. add custom favicon

--- src/app/(home)/_components/HomeHero.tsx
+++ src/app/(home)/_components/HomeHero.tsx
@@ -1,23 +1,7 @@
 'use client'
-import { useEffect, useState } from "react";
 import Image from "next/image";
 
-const BACKGROUNDS = [
-    "/background-1.jpg",
-    "/background-2.webp",
-    "/background-3.avif",
-    "/background-4.avif",
-    "/background-5.webp",
-    "/background-6.png",
-];
 export default function HomeHero() {
-    const [backgroundImage, setBackgroundImage] = useState<string>("");
-    useEffect(() => {
-        const index: number = Math.floor(Math.random() * BACKGROUNDS.length);
-        const selectedImage = BACKGROUNDS[index];
-        setBackgroundImage(selectedImage);
-        return () => { };
-    }, []);
     return (
         <>
             <h1 className="md:text-8xl text-5xl py-4">PROJECT MAL</h1>
@@ -25,7 +9,7 @@ export default function HomeHero() {
                 <div className="w-full h-full absolute backdrop-blur-sm z-10 flex items-center justify-center rounded-xl">
                     <p className="md:text-7xl text-4xl text-white font-bold app-title">Discover New Anime</p>
                 </div>
-                {backgroundImage ? <Image src={backgroundImage} sizes="(max-width: 768px)50vw, 33vw" fill className="object-cover md:rounded-2xl" priority alt="background-image" /> : null}
+                <Image src={'/lp-background.jpg'} sizes="(max-width: 768px)50vw, 25vw" quality={50} fill className="object-cover md:rounded-2xl" priority fetchPriority="high" alt="background-image" />
             </figure>
         </>
     )
