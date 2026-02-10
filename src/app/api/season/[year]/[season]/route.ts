51ce41ee94577a637ccd648db1d882187151e3e7
nhl1991 <laslark1991@gmail.com>
2026-02-10 23:22:16 +0900

    Fix: Season Animation Route.

--- src/app/api/season/[year]/[season]/route.ts
+++ src/app/api/season/[year]/[season]/route.ts
@@ -3,13 +3,14 @@ import { getAnimations } from '@/lib/fetchAnimation';
 import { NextRequest, NextResponse } from 'next/server';
 
 const LIMIT = 16;
-const SEASON = ['winter', 'spring', 'summer', 'fall'];
-export async function GET(req: NextRequest, ctx: RouteContext<'/api/season/[season]/[year]'>) {
-  const { season, year } = await ctx.params
+const SEASONTYPE = ['winter', 'spring', 'summer', 'fall'];
+export async function GET(req: NextRequest, { params }: { params: Promise<{ year: string; season: string }>}) {
+  const {  year, season } = await params
   const offset = req.nextUrl.searchParams.get('offset') ?? 0;
-  const query = `anime/season/${season}/${year}?offset=${offset}&limit=${LIMIT}&sort=anime_num_list_users`
+  const query = `anime/season/${year}/${season}?offset=${offset}&limit=${LIMIT}&sort=anime_num_list_users`
+  
   if (!season) return NextResponse.json({ error: 'Bad Request', message: 'Missing required parameter: season' }, { status: 400 });
-  if(!SEASON.includes(season as string)) return NextResponse.json({ error: 'Bad Request', message: 'Wrong parameter: season.' }, { status: 400 });
+  if(!SEASONTYPE.includes(season as string)) return NextResponse.json({ error: 'Bad Request', message: 'Wrong parameter: season.' }, { status: 400 });
   if (!year) return NextResponse.json({ error: 'Bad Request', message: 'Missing required parameter: year' }, { status: 400 });
   try {
     const response = await getAnimations(query, "season");
